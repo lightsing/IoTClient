9e6e2c018fb9cf75c88561bd935ffbbc3ecbefda
lightsing <light.tsing@gmail.com>
2017-02-28 00:19:52 +0800

    Update GPIO Map

---
+++ config_example.h
@@ -0,0 +1,26 @@
+#ifndef X_CONFIG_H_   /* Include guard */
+#define X_CONFIG_H_
+
+#define ssid "Wi-Fi SSID"
+#define password "Wi-Fi Password"
+
+#define host "IP or Domain Name"
+#define port PortNum
+
+#define deviceName "Device Name"
+// SHA1 fingerprint of the certificate
+#define fingerprint "00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"
+
+#define D0 16
+#define D1  5
+#define D2  4
+#define D3  0
+#define D4  2
+#define D5 14
+#define D6 12
+#define D7 13
+#define D8 15
+#define D9  3
+#define D10 1
+
+#endif
